56e2d8e4f639100b77fa9daf9d40e29e08b7201a
Zsolt Domokos <zdomokos@gmail.com>
2022-05-15 12:26:48 -0400

    Flex: WashSale simplified

--- IBNet/Flex/IbFlexReader.Contracts/Ib/WashSale.cs
+++ IBNet/Flex/IbFlexReader.Contracts/Ib/WashSale.cs
@@ -3,43 +3,18 @@ using System.ComponentModel;
 namespace IbFlexReader.Contracts.Ib
 {
     [TypeConverter(typeof(ExpandableObjectConverter))]
-    public class WashSale
+    public class WashSale : SecurityInfo
     {
         public string AccountId                 { get; set; }
         public string AcctAlias                 { get; set; }
         public string Model                     { get; set; }
-        public string Currency                  { get; set; }
         public string FxRateToBase              { get; set; }
-        public string AssetCategory             { get; set; }
-        public string Symbol                    { get; set; }
-        public string Description               { get; set; }
-        public string Conid                     { get; set; }
-        public string SecurityID                { get; set; }
-        public string SecurityIDType            { get; set; }
-        public string Cusip                     { get; set; }
-        public string Isin                      { get; set; }
-        public string ListingExchange           { get; set; }
-        public string UnderlyingConid           { get; set; }
-        public string UnderlyingSymbol          { get; set; }
-        public string UnderlyingSecurityID      { get; set; }
-        public string UnderlyingListingExchange { get; set; }
-        public string Issuer                    { get; set; }
-        public string Multiplier                { get; set; }
-        public string Strike                    { get; set; }
-        public string Expiry                    { get; set; }
         public string TradeID                   { get; set; }
-        public string PutCall                   { get; set; }
         public string ReportDate                { get; set; }
-        public string PrincipalAdjustFactor     { get; set; }
         public string DateTime                  { get; set; }
-        public string SerialNumber              { get; set; }
         public string TradeDate                 { get; set; }
-        public string DeliveryType              { get; set; }
-        public string CommodityType             { get; set; }
         public string SettleDateTarget          { get; set; }
-        public string Fineness                  { get; set; }
         public string TransactionType           { get; set; }
-        public string Weight                    { get; set; }
         public string Exchange                  { get; set; }
         public string Quantity                  { get; set; }
         public string TradePrice                { get; set; }
